a52cf5c28292287fd43405a41765e4afc3fa5da0
khushwant <khushu36@gmail.com>
2019-11-23 13:12:38 +0530

    starting of django

--- folder2/main.py
+++ folder2/main.py
@@ -6,5 +6,25 @@ print(req.status_code)
 # print(req.content)
 
 bs = BeautifulSoup(req.content,'html.parser')
+# print(bs.prettify())
+fp = open("fileName.csv","w")
+fp.write('t-shirt , price , image')
 
-print(bs.prettify())
+
+for i in bs.find_all('div',{'class' : 'productCardBox'}):
+    for Tname in bs.find_all('div',{'class' : 'productCarDetail'}):
+        
+        print(str(Tname.find_all('h3')[0].text))
+        print(str(Tname.find_all('b')[0].text))
+        fp.write(str(Tname.find_all('h3')[0].text))
+        fp.write(',')
+        fp.write(str(Tname.find_all('b')[0].text))
+        fp.write(',')
+
+        
+    for img in bs.find_all('div',{'class' : 'productCardImg'}):
+        fp.write(str(img.find_all('img').text))
+        fp.write('\n')
+
+
+
